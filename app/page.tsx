4b9ed64522e2a9e8609db3107a7cc39a37d2a37a
Nick Chakre <nickchakre18@gmail.com>
2025-08-09 20:50:44 -0400

    Added basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold">Flight Tracker</h1>
+      <h1 className="text-3xl font-bold text-left border-2">Flight Tracker</h1>
+      <div>
+      <h2 className="text-3xl font-bold text-left border-2 pt-20 pl-30 flex w-100 max-h-full">Your Flight Details</h2>
+      <div className="w-100 h-100 bg-blue-500"></div>
+      </div>
+      <h3 className="text-2xl font-bold text-center border-2">New Flights for you!</h3>
     </main>
   )
 }
