feb6a5190ba563d065bbd535dd5dba2e4999a87e
Nick Chakre <nickchakre18@gmail.com>
2025-08-11 14:35:43 -0400

    Added Convex Backend

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
   <main>
     <div className="bg-cyan-950 h-20 flex justify-between items-center">
-      <h1 className="text-2xl font-bold bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent border-2 border-black"> Flight Guard AI</h1>
+      <h1 className="text-2xl font-bold bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent"> Flight Guard AI</h1>
     </div>
 
   <div className="bg-blue-950 border-2 border-black h-max flex flex-col gap-6 p-6 items-center justify-center">
